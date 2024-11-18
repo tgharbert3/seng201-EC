b55ef4264260bc65248af8bc8021beedf1644df0
Tyler Harbert <tyler.g.harbert@gmail.com>
2024-11-17 20:18:48 -0500

    merged new-feature

--- math_ops.py
+++ math_ops.py
@@ -11,8 +11,17 @@ def divide_numbers(a, b):
         quit()
         
 
+def multiply_number(a,b):
+    """Mutlplies two numbers"""
+    return a * b
+
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
     print(f"The result of division is: {result}")
+
+    a = 10
+    b = 5
+    result = multiply_number(a, b)
+    print(f"The result of multiply is: {result}")
