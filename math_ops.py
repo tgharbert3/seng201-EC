8881a414cbb6f538c04adcf4ce1b4cb57831233f
Tyler Harbert <tyler.g.harbert@gmail.com>
2024-11-17 20:13:45 -0500

    added multipy_numbers

--- math_ops.py
+++ math_ops.py
@@ -6,8 +6,17 @@ def divide_numbers(a, b):
     """Divides two numbers and returns the result."""
     return a / b  # Potential division by zero error
 
+def multiply_number(a,b):
+    """Mutlplies two numbers"""
+    return a * b
+
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
     print(f"The result of division is: {result}")
+
+    a = 10
+    b = 5
+    result = multiply_number(a, b)
+    print(f"The result of multiply is: {result}")
